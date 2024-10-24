3ff6135fd62ff62a2a45ad65fae0c5a1ac4ef11e
mynampujitha <mynampujitha@gmail.com>
2024-10-24 11:58:46 +0530

    Update hello.sh
    Hello Team there was a bug and now its cleared. Kindly update on your local repo

--- hello.sh
+++ hello.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 echo "Hi and it's rainy dayi"
 echo "welcome to the Devops Sessions"
+#!/bin/bash
+echo "Hi and it's rainy dayi"
+echo "welcome to the Devops Sessions"
+#!/bin/bash
+echo "Hi and it's rainy dayi"
+echo "welcome to the Devops Sessions"
+#!/bin/bash
+echo "Hi and it's rainy dayi"
+echo "welcome to the Devops Sessions"
+#!/bin/bash
+echo "Hi and it's rainy dayi"
+echo "welcome to the Devops Sessions"
+
+
 
