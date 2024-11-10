ef9db1d39c30b41693cf70ad399f4bdef27d05a0
mynampujitha <mynampujitha@gmail.com>
2024-11-10 16:45:31 +0530

    Update hello.sh
    this is my frist file

--- hello.sh
+++ hello.sh
@@ -7,6 +7,14 @@ echo "welcome to the Devops Sessions"
 #!/bin/bash
 echo "Hi and it's rainy dayi"
 echo "welcome to the Devops Sessions"
+#!/bin/bash
+echo "Hi and it's rainy dayi"
+echo "welcome to the Devops Sessions"
+#!/bin/bash
+echo "Hi and it's rainy dayi"
+echo "welcome to the Devops Sessions"
+
+
 
 
 
